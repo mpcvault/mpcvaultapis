a726efa8d60a4db61a554a8b2d059518e565ca39
Jason <lxjpub@gmail.com>
2024-03-05 20:42:29 -0800

    update proto

--- examples/golang-send-usdc/main.go
+++ examples/golang-send-usdc/main.go
@@ -14,6 +14,8 @@ import (
 
 const APIEndpoint string = "api.mpcvault.com:443"
 const APIToken = "[API Token]"
+const VaultUUID = "[vault uuid]"
+const CallbackClientSignerPublicKey = "[callback client signer public key]"
 
 var grpcClient apipb.PlatformAPIClient
 
@@ -48,8 +50,35 @@ func EVMSendERC20(ctx context.Context) {
 		TokenContractAddress: "0x2791bca1f2de4661ed88a30c99a7a9449aa84174", // USDC contract address on polygon
 		Amount:               "1000000",                                    // 1 USDC
 		GasFee:               nil,                                          // leave nil to use auto gas settings
+		Nonce:                &wrappers.Int64Value{Value: 0},
 	}
 	req.Notes = &wrappers.StringValue{Value: "sending 1 USDC for testing"} // setting transaction notes
+
+	res, err := grpcClient.CreateSigningRequest(ctx, &req)
+	if err != nil {
+		log.Fatalf("err: %v", err)
+	}
+
+	b, _ := json.MarshalIndent(res, "", " ")
+	fmt.Println(string(b))
+}
+
+// EVMSendERC20 send erc20 token with client signer
+func EVMSendERC20WithClientSigner(ctx context.Context) {
+	req := apipb.CreateSigningRequestRequest{}
+	createSigningType := &apipb.CreateSigningRequestRequest_EvmSendErc20{}
+	req.Type = createSigningType
+	createSigningType.EvmSendErc20 = &apipb.EVMSendERC20{
+		ChainId:              137,                                          // polygon chain id
+		From:                 "0x71C7656EC7ab88b098defB751B7401B5f6d8976F", // sender address
+		To:                   "0x71C7656EC7ab88b098defB751B7401B5f6d8976F", // receiver address
+		TokenContractAddress: "0x2791bca1f2de4661ed88a30c99a7a9449aa84174", // USDC contract address on polygon
+		Amount:               "1000000",                                    // 1 USDC
+		GasFee:               nil,                                          // leave nil to use auto gas settings
+	}
+	req.Notes = &wrappers.StringValue{Value: "sending 1 USDC for testing"} // setting transaction notes
+	req.VaultUuid = &wrappers.StringValue{Value: VaultUUID}
+	req.CallbackClientSignerPublicKey = &wrappers.StringValue{Value: CallbackClientSignerPublicKey}
 
 	res, err := grpcClient.CreateSigningRequest(ctx, &req)
 	if err != nil {
--- examples/python-deploy-contract/main.py
+++ examples/python-deploy-contract/main.py
@@ -3,8 +3,11 @@ import mpcvaultapis.mpcvault.platform.v1.api_pb2 as pb
 import mpcvaultapis.mpcvault.platform.v1.api_pb2_grpc as pb_grpc
 from google.protobuf.wrappers_pb2 import StringValue
 
-CONTRACT = "79204861726468617420546f6b656e0............" # hex encoded contract bytecode
+CONTRACT = "79204861726468617420546f6b656e0............"  # hex encoded contract bytecode
 API_TOKEN = "[YOUR API TOKEN]"
+VAULT_UUID = "[vault uuid]"
+CALLBACK_CLIENT_SIGNER_PUBLIC_KEY = "[callback client signer public key]"
+
 
 class GRPCAuth(grpc.AuthMetadataPlugin):
     def __init__(self, key):
@@ -13,23 +16,26 @@ class GRPCAuth(grpc.AuthMetadataPlugin):
     def __call__(self, context, callback):
         callback((('x-mtoken', self._key),), None)
 
+
 if __name__ == '__main__':
-    channel = grpc.secure_channel('api.mpcvault.com:443', 
+    channel = grpc.secure_channel('api.mpcvault.com:443',
                                   grpc.composite_channel_credentials(
                                       grpc.ssl_channel_credentials(),
                                       grpc.metadata_call_credentials(GRPCAuth(API_TOKEN)),
                                   ))
     stub = pb_grpc.PlatformAPIStub(channel)
 
-    gas_fee = pb.EVMGas(gas_limit=StringValue(value="10000000")) # you can leave other fields empty to use default gas settings
+    gas_fee = pb.EVMGas(
+        gas_limit=StringValue(value="10000000"))  # you can leave other fields empty to use default gas settings
 
-    response = stub.CreateSigningRequest(pb.CreateSigningRequestRequest(evm_send_custom = pb.EVMSendCustom(**{
-            "chain_id":137, #polygon
-            "from":"0xc0abaa254729296a45a3885639AC7E10F9d54979", # your wallet address on MPCVault
-            "to":"", # leave empty for contract creation 
-            "value":"0", # amount of tokens to send, in this case 0
-            "input":bytes.fromhex(CONTRACT), # contract bytecode
-            "gas_fee":gas_fee, # you can leave this empty to use default gas settings
-        })))
-    
+    response = stub.CreateSigningRequest(pb.CreateSigningRequestRequest(evm_send_custom=pb.EVMSendCustom(**{
+        "chain_id": 137,  # polygon
+        "from": "0xc0abaa254729296a45a3885639AC7E10F9d54979",  # your wallet address on MPCVault
+        "to": "",  # leave empty for contract creation
+        "value": "0",  # amount of tokens to send, in this case 0
+        "input": bytes.fromhex(CONTRACT),  # contract bytecode
+        "gas_fee": gas_fee,  # you can leave this empty to use default gas settings
+        "nonce": StringValue(value="0"),  # you can leave this empty to use default nonce
+    })))
+
     print("Received message: ", response)
